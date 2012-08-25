38bcbb10cdff1df4ba5c362d099bf03f13e8cbd0
Marrselo <marrselo@gmail.com>
2012-08-25 01:37:33 -0500

    marcelo

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,7 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+        
     public function __construct($app) {
         parent::__construct($app);
         date_default_timezone_set('America/Lima');
