9492bf0dd27236ea08b22c3c726520db7c28e737
nazart jara huaman <nazarjb@hotmail.com>
2012-08-25 03:07:49 -0500

    cambios nazart

--- application/modules/default/controllers/CotizarController.php
+++ application/modules/default/controllers/CotizarController.php
@@ -1,18 +1,15 @@
 <?php
 
-class Default_CotizarController extends CST_Controller_ActionDefault
-{
+class Default_CotizarController extends CST_Controller_ActionDefault {
 
-    public function init()
-    {
+    public function init() {
         parent::init();
-        $this->view->headLink()->appendStylesheet(CST_Server_ServerStatic::getUrl().'/css/cotizacion.css');
+        $this->view->headLink()->appendStylesheet(CST_Server_ServerStatic::getUrl() . '/css/cotizacion.css');
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         $this->view->productoCotizar = $this->_sessiondefault->cotizacion;
-                $form = new Application_Form_Cotizar();
+        $form = new Application_Form_Cotizar();
         $this->view->flashMessenger = $this->_flashMessenger->getMessages();
         if ($this->_request->isPost()) {
             if ($form->isValid($this->_getAllParams())) {
@@ -26,34 +23,32 @@ class Default_CotizarController extends CST_Controller_ActionDefault
                 $data['_descripcion'] = $paramCotizacion['descripcion'];
                 $cotizacion->setProperties($data);
                 $productos = new Application_Entity_Producto();
-                foreach($this->_sessiondefault->cotizacion as $index){
-                    $productos->setProperties($index);
-                    $cotizacion->insertarProducto($producto);
+                if (!empty($this->_sessiondefault->cotizacion)) {
+                    foreach ($this->_sessiondefault->cotizacion as $index) {
+                        $productos->setProperties($index);
+                        $cotizacion->insertarProducto($producto);
+                    }
                 }
                 $cotizacion->insertContizacion();
-                
-                
-                
-                
-               $this->_flashMessenger->addMessage('La cotizacion se registro correctamente: En breve nos estaremos poniendo en contacto con usted Gracias!');
-               $this->_redirect('/cotizar');
-            }else{
+                $this->_flashMessenger->addMessage('La cotizacion se registro correctamente: En breve nos estaremos poniendo en contacto con usted Gracias!');
+                $this->_redirect('/cotizar');
+            } else {
                 $this->_flashMessenger->addMessage('Corriga los datos');
             }
         }
         $this->view->formulario = $form;
-        
     }
-    public function productoAction(){
+
+    public function productoAction() {
         $productos = new Application_Entity_Producto();
         $productos->searchProducto($this->_getParam('slugProducto'));
-        $this->_sessiondefault->cotizacion[$productos->_slugProducto] =  $productos->getProducto();
-        $this->_redirect('/cotizar');
-    }
-    public function deleteAction(){
-        unset($this->_sessiondefault->cotizacion[$this->_getParam('slugProducto')]);
+        $this->_sessiondefault->cotizacion[$productos->_slugProducto] = $productos->getProducto();
         $this->_redirect('/cotizar');
     }
 
+    public function deleteAction() {
+        unset($this->_sessiondefault->cotizacion[$this->_getParam('slugProducto')]);
+        $this->_redirect('/cotizar');
+    }
 
 }
