dbd37cf21ef0a9233d06369ff0f0b20fd1683e3a
makuproyect <celsodiaz90@gmail.com>
2012-08-25 19:26:34 -0500

    CELSOINFOBD

---
+++ bd/TABLAS/categoriaproducto.sql
@@ -0,0 +1,22 @@
+/*
+SQLyog Enterprise - MySQL GUI
+MySQL - 5.5.27 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `categoriaproducto` (
+	`idCategoriaProducto` double ,
+	`NombreCategoriaProducto` varchar (600),
+	`flagActivoCategoriaProducto` double ,
+	`IdArea` double ,
+	`SlugCategoriaProducto` varchar (600)
+); 
+insert into `categoriaproducto` (`idCategoriaProducto`, `NombreCategoriaProducto`, `flagActivoCategoriaProducto`, `IdArea`, `SlugCategoriaProducto`) values('1','Servidores','1','1','servidores');
+insert into `categoriaproducto` (`idCategoriaProducto`, `NombreCategoriaProducto`, `flagActivoCategoriaProducto`, `IdArea`, `SlugCategoriaProducto`) values('2','Storage','1','1','storage');
+insert into `categoriaproducto` (`idCategoriaProducto`, `NombreCategoriaProducto`, `flagActivoCategoriaProducto`, `IdArea`, `SlugCategoriaProducto`) values('3','Desktop','1','1','desktop');
+insert into `categoriaproducto` (`idCategoriaProducto`, `NombreCategoriaProducto`, `flagActivoCategoriaProducto`, `IdArea`, `SlugCategoriaProducto`) values('4','Portatiles','1','1','portatiles');
+insert into `categoriaproducto` (`idCategoriaProducto`, `NombreCategoriaProducto`, `flagActivoCategoriaProducto`, `IdArea`, `SlugCategoriaProducto`) values('5','Impresoras','1','1','impresoras');
+insert into `categoriaproducto` (`idCategoriaProducto`, `NombreCategoriaProducto`, `flagActivoCategoriaProducto`, `IdArea`, `SlugCategoriaProducto`) values('6','Comunicaciones','1','1','comunicaciones');
+insert into `categoriaproducto` (`idCategoriaProducto`, `NombreCategoriaProducto`, `flagActivoCategoriaProducto`, `IdArea`, `SlugCategoriaProducto`) values('7','Accesorios','1','1','accesorios');
+insert into `categoriaproducto` (`idCategoriaProducto`, `NombreCategoriaProducto`, `flagActivoCategoriaProducto`, `IdArea`, `SlugCategoriaProducto`) values('8',NULL,NULL,NULL,NULL);
---
+++ bd/TABLAS/marcas.sql
@@ -0,0 +1,22 @@
+/*
+SQLyog Enterprise - MySQL GUI
+MySQL - 5.5.27 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `marca` (
+	`IdMarca` double ,
+	`NombreMarca` varchar (600),
+	`SlugMarca` varchar (600),
+	`ImagenMarca` varchar (600)
+); 
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('1','HP','hp','1.jpg');
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('2','IBM','ibm','2.jpg');
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('3','EMC','emc','3.jpg');
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('4','LENOVO','lenovo','4.jpg');
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('5','TOSHIBA','toshiba','5.jpg');
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('6','OKIDATA','okidata','6.jpg');
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('7','CISCO','cisco','7.jpg');
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('8','FORTY','forty','8.jpg');
+insert into `marca` (`IdMarca`, `NombreMarca`, `SlugMarca`, `ImagenMarca`) values('9','ESET','eset','9.jpg');
